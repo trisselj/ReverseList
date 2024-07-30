580d4ab3e99fb6eac61ef10a8bf1d963a2a19f55
trisselj <trisselj@oregonstate.edu>
2024-07-30 16:21:20 -0700

    q

--- reverse_list.py
+++ reverse_list.py
@@ -4,10 +4,10 @@
 # Description: reverses the order of the elements in a given list
 
 # Defining what will reverse the order of the elements 
-def reverse_list(1st):
+def reverse_list(first):
     start = 0 # Iniitializes start at beginning of list
-    end = len(1st) - 1 # Initializes end at end of list
+    end = len(first) - 1 # Initializes end at end of list
     while start < end: # Continues as long as start value is < end value
-        1st[start], 1st[end] = 1st[end], 1st[start] # Swapping start and end
+        first[start], first[end] = first[end], first[start] # Swapping start and end
         start += 1 # Moves start forward
         end -= 1 # Moves end backward
